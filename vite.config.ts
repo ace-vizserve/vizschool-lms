72885c7f56305361c9a6d4fcb499c78bcd2402ce
ace.vizserve <ace.vizserve@gmail.com>
2025-04-04 12:52:17 +0800

    chore: update vite version to latest & add tailwind-shadcn prefix (tw:)

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-      },
     },
   },
 })
